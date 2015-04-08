8fb58385079bbd060d69fea083b3ccb68eca361a
Prathmesh Bodas <prathmesh@gmail.com>
2015-04-08 21:27:13 +0530

    What if future.get() not called

--- RoughWork/Source.cpp
+++ RoughWork/Source.cpp
@@ -4,9 +4,18 @@
 
 using namespace std;
 
+void foo()
+{
+	cout << "Start Worker:" << endl;
+	this_thread::sleep_for(std::chrono::seconds(15));
+	cout << "End Worker:" << endl;
+}
 
 int main()
 {
-	cout << "Hello World" << endl;
-	cin.get();
+	cout << "Inside main: " << endl;
+	future<void> fut = async(std::launch::async, foo);
+
+	this_thread::sleep_for(std::chrono::seconds(2));
+	cout << "End of main: " << endl;
 }
